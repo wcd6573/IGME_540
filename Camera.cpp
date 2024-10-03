0082809cbde8be7fbf7b9e56851e11ab96c4c89a
wcd6573 <wcd6573@rit.edu>
2024-10-03 16:18:34 -0400

    Filled out the Camera header file and wrote the constructor.

--- Camera.cpp
+++ Camera.cpp
@@ -6,3 +6,134 @@ Camera Implementation
 
 #include "Camera.h"
 #include "Input.h"
+using namespace DirectX;
+
+Camera::Camera(
+    XMFLOAT3 startPosition, 
+    float _aspectRatio, 
+    float _fov, 
+    float _nearClip, 
+    float _farClip, 
+    bool _doPerspective) :
+        aspectRatio(_aspectRatio),
+        fov(_fov),
+        nearClip(_nearClip),
+        farClip(_farClip),
+        doPerspective(_doPerspective)
+{
+    // Create transform and set starting position
+    transform = std::make_shared<Transform>();
+    transform->SetPosition(startPosition);
+
+    // Calculate view and projection matrices
+    UpdateViewMatrix();
+    UpdateProjectionMatrix(aspectRatio);
+}
+
+Camera::~Camera()
+{
+
+}
+
+void Camera::Update(float dt)
+{
+
+}
+
+void Camera::UpdateViewMatrix()
+{
+
+}
+
+void Camera::UpdateProjectionMatrix(float aspectRatio)
+{
+
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+// ------------------------------- GETTERS --------------------------------- //
+///////////////////////////////////////////////////////////////////////////////
+XMFLOAT4X4 Camera::GetViewMatrix()
+{
+    return DirectX::XMFLOAT4X4();
+}
+
+XMFLOAT4X4 Camera::GetProjectionMatrix()
+{
+    return DirectX::XMFLOAT4X4();
+}
+
+std::shared_ptr<Transform> Camera::GetTransform()
+{
+    return std::shared_ptr<Transform>();
+}
+
+float Camera::GetAspectRatio()
+{
+    return 0.0f;
+}
+
+float Camera::GetFieldOfView()
+{
+    return 0.0f;
+}
+
+float Camera::GetNearClip()
+{
+    return 0.0f;
+}
+
+float Camera::GetFarClip()
+{
+    return 0.0f;
+}
+
+float Camera::GetMoveSpeed()
+{
+    return 0.0f;
+}
+
+float Camera::GetLookSpeed()
+{
+    return 0.0f;
+}
+
+bool Camera::GetProjectionType()
+{
+    return false;
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+// ------------------------------- SETTERS --------------------------------- //
+///////////////////////////////////////////////////////////////////////////////
+void Camera::SetFieldOfView(float _fov)
+{
+
+}
+
+void Camera::SetNearClip(float _nearClip)
+{
+
+}
+
+void Camera::SetFarClip(float _farClip)
+{
+
+}
+
+void Camera::SetMoveSpeed(float _moveSpeed)
+{
+
+}
+
+void Camera::SetLookSpeed(float _lookSpeed)
+{
+
+}
+
+void Camera::SetProjectionType(bool _doPerspective)
+{
+
+}
--- Camera.h
+++ Camera.h
@@ -5,7 +5,65 @@ Camera Header
 */
 
 #pragma once
+#include <DirectXMath.h>
+#include <memory>
+#include "Transform.h"
+
+// --------------------------------------------------------
+// A class representing a camera. It contains view
+// and projection matrices, as well as other fields
+// for controlling how the scene is viewed.
+// --------------------------------------------------------
 class Camera
 {
+public:
+	// Camera takes a start position so that
+	// it doesn't always start at the origin
+	Camera(DirectX::XMFLOAT3 startPosition, 
+		float _aspectRatio,
+		float _fov = 60.0f,			// Give default values
+		float _nearClip = 0.01f,	// for some fields 
+		float _farClip = 100.0f,
+		bool _doPerspective = true);
+	~Camera();
 
+	// Update methods
+	void Update(float dt);
+	void UpdateViewMatrix();
+	void UpdateProjectionMatrix(float aspectRatio);
+
+	// Getters
+	DirectX::XMFLOAT4X4 GetViewMatrix();
+	DirectX::XMFLOAT4X4 GetProjectionMatrix();
+	std::shared_ptr<Transform> GetTransform();
+	float GetAspectRatio();
+	float GetFieldOfView();
+	float GetNearClip();
+	float GetFarClip();
+	float GetMoveSpeed();
+	float GetLookSpeed();
+	bool GetProjectionType();
+
+	// Setters
+	void SetFieldOfView(float _fov);
+	void SetNearClip(float _nearClip);
+	void SetFarClip(float _farClip);
+	void SetMoveSpeed(float _moveSpeed);
+	void SetLookSpeed(float _lookSpeed);
+	void SetProjectionType(bool _doPerspective);
+
+private:
+	// Basic fields
+	std::shared_ptr<Transform> transform;
+	DirectX::XMFLOAT4X4 viewMatrix;
+	DirectX::XMFLOAT4X4 projMatrix;
+	float aspectRatio;
+
+	// Extra customization
+	float fov;		// in radians
+	float nearClip;	// near clip plane distance
+	float farClip;	// far clip plane distance
+	float moveSpeed;	// How fast you move around
+	float lookSpeed;	// How fast you can rotate the camera
+	bool doPerspective;	// perspective, or orthographic projection
 };
