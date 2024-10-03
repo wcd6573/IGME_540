9ba62d11db5b55f45183baf5d16c0d05545a650b
wcd6573 <wcd6573@rit.edu>
2024-10-03 00:23:48 -0400

    Stubbed out the Camera class.

---
+++ Camera.cpp
@@ -0,0 +1,8 @@
+/*
+William Duprey
+10/3/24
+Camera Implementation
+*/
+
+#include "Camera.h"
+#include "Input.h"
---
+++ Camera.h
@@ -0,0 +1,11 @@
+/*
+William Duprey
+10/3/24
+Camera Header
+*/
+
+#pragma once
+class Camera
+{
+
+};
